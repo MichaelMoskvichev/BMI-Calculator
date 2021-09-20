3773b57b1ec06412d0c96976fa0691bc43b35e5e
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2021-09-20 10:20:53 +0300

    end project

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Миша Москвичев on 27.11.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value : Float
+    let advice : String
+    let color : UIColor
+}
